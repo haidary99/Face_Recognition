07c922950b8431e9619afeb2559569376c9e9cbc
haidary99 <haidary181@gmail.com>
2023-08-08 20:05:20 +0300

    Added direct_use and learn_features scripts. 1. direct_use is for avoiding the hassle of having to learn and extract the features of all the faces everytime the program is run. 2. learn_features is used to learn the features and embeddings of features then store this information in the data.pt file which then loading using the former script

--- direct_use.py
+++ direct_use.py
@@ -1,4 +1,10 @@
-# This file is used to directly utilize `InceptionResnetV1` in order to compare face(s) in frame with saved embeddings in `data.pt`. This file does not train on the available dataset of faces! You need to use `face_reco.ipynb` for that purpose.
+"""
+This file is used to directly utilize `InceptionResnetV1` in order to compare face(s) in 
+frame with saved embeddings in `data.pt`.
+This file does not train on the available dataset of faces! You need to use `learn_featurs.py`
+for that purpose.
+"""
+
 
 from facenet_pytorch import MTCNN, InceptionResnetV1
 import torch
@@ -12,18 +18,54 @@ import pandas as pd
 mtcnn = MTCNN(image_size=240, margin=0, keep_all=True, min_face_size=300) # keep_all = True keeps all faces in a form of list, Also changed mmin_face_size to 150 to ignore small faces in frame which might be difficult to classify
 resnet = InceptionResnetV1(pretrained='vggface2').eval() # creating an instance of pre-trained InceptionResnetV1 model trained on the VGGFace2 dataset to extract embeddings
 
+import pandas as pd
+import time
+from datetime import timedelta, datetime, date
+
+def update_status(row):
+    '''
+    Change status from absent to present or departed based on other column values
+    '''
+    if pd.notna(row['arrival_date']):
+        if pd.notna(row['departure_date']):
+            # print("changed status to departed")
+            return 'departed'
+        # print("changed status to present")
+        return 'present'
+    else:
+        # print("No change in status")
+        return row['status']
+
+
+def update_time_diff(row):
+    '''
+    calculate time_difference column based on other column values
+    '''
+    if pd.notna(row['arrival_date']):
+        if pd.notna(row['departure_date']):
+            time_format = "%H:%M:%S"
+            arrival_datetime = datetime.strptime(row['arrival_time'], time_format)
+            departure_datetime = datetime.strptime(row['departure_time'], time_format)
+            time_diff = (departure_datetime - arrival_datetime).seconds
+            time_diff_seconds = timedelta(seconds=time_diff)
+            return time_diff_seconds
+    else:
+        # print("No change in status")
+        return row['time_difference']
+
+
 # in case program stops working on a specific day and is reloaded, the old info should not be overwritten! therefore we define the below function:
 # Function to save attendance data to an Excel file with the current date in the file name
 def save_attendance_to_excel(df_attendance):
     today = date.today().strftime('%Y-%m-%d')
-    excel_file = f'attendance_{today}.xlsx'
+    excel_file = f'attendance_{today}.csv'
 
     if os.path.exists(excel_file):
         # If the file already exists, read its content into a DataFrame
-        existing_df = pd.read_excel(excel_file)
-
+        existing_df = pd.read_csv(excel_file)
+        # print(f"\nExisting file:\n{existing_df}\n")
         # Merge the existing DataFrame with the new attendance data based on the "Name" column
-        updated_df = df_attendance.merge(existing_df, on="Name", how="left", suffixes=('', '_new'))
+        updated_df = existing_df.merge(df_attendance, on="Name", how="left", suffixes=('', '_new'))
 
         # Replace the existing columns with the new columns where there are no missing values
         for col in df_attendance.columns:
@@ -33,12 +75,21 @@ def save_attendance_to_excel(df_attendance):
         # Drop the temporary columns with "_new" suffix
         updated_df = updated_df.drop([col for col in updated_df.columns if col.endswith('_new')], axis=1)
 
+        # Change status from absent to present or departed based on other column values
+        updated_df["status"] = updated_df.apply(update_status, axis=1)
+
+        # Calculate time_difference column based on other column values
+        updated_df["time_difference"] = updated_df.apply(update_time_diff, axis=1)
+
         # Save the updated DataFrame back to the Excel file, overwriting its content
-        updated_df.to_excel(excel_file, index=False)
+        updated_df.to_csv(excel_file, index=False)
+
+        # print(f"\ndf_attendnace:\n {df_attendance}\n")
+        # print(f"\nupdated_df:\n {updated_df}\n")
 
     else:
         # If the file does not exist, save the attendance data to a new Excel file
-        df_attendance.to_excel(excel_file, index=False)
+        df_attendance.to_csv(excel_file, index=False)
 
 
 # Loading data.pt file
@@ -57,7 +108,7 @@ cam = cv2.VideoCapture(2)  # initializing the camera from CV, 0 means the defaul
 # cam.open(address)
 
 names = [name.replace('_', ' ') for name in name_list]
-attendance_dict = {name: {"status": "absent", "arrival_date": "", "arrival_time": "", "departure_date": "", "departure_time": "", "time_difference": ""} for name in names} # Initialize attendance dictionary which has all the names in the db as keys and another dictionary as value
+attendance_dict = {name: {"status": "absent", "arrival_date": None, "arrival_time": None, "departure_date": None, "departure_time": None, "time_difference": None} for name in names} # Initialize attendance dictionary which has all the names in the db as keys and another dictionary as value
 
 # Initialize counter and maximum consecutive frames
 consecutive_frames_dict = {name: 0 for name in names} # all names in db as keys and 0 for their values
@@ -71,8 +122,8 @@ df_attendance.reset_index(inplace=True)
 df_attendance.rename(columns={'index': 'Name'}, inplace=True)
 
 # Print the DataFrame
-print("\nAttendance information before program starts:")
-print(df_attendance)
+# print("\nAttendance information before program starts:")
+# print(df_attendance)
 
 present_time_dict = {name: None for name in names} # used to calculate time diff. between arrival and departure
 
@@ -183,9 +234,11 @@ while True: # read all the frames in the video
                                 arrival_datetime = datetime.strptime(arrival_time, time_format)
                                 departure_datetime = datetime.strptime(departure_time, time_format)
                                 time_difference_seconds = (departure_datetime - arrival_datetime).seconds
+                                print(f"time_difference_seconds: {time_difference_seconds}")
 
                                 # Add the "time_difference" key to the attendance_dict
                                 attendance_dict[name]["time_difference"] = timedelta(seconds=time_difference_seconds)
+                                print(f"timedelta: {attendance_dict[name]['time_difference']}")
 
                                 # Update the DataFrame with the latest attendance information
                                 df_attendance["status"] = df_attendance["Name"].map(lambda name: attendance_dict[name]["status"])
@@ -222,7 +275,7 @@ while True: # read all the frames in the video
                         frame = cv2.putText(frame, f"{name} - {status_text}", (int(box[0])-5, int(box[1])-12), cv2.FONT_HERSHEY_SIMPLEX, 1, status_color, 2, cv2.LINE_AA)
                         frame = cv2.rectangle(frame, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), status_color, 3)
 
-    cv2.imshow("Face Recognition", frame)
+    cv2.imshow("IMG", frame)
 
     # If employee forgot to make departure (remained as arrived), mark him back as absent
     for name, Bool in bool_dict.items():
@@ -267,8 +320,8 @@ while True: # read all the frames in the video
 cam.release()
 
 # Print the DataFrame
-print("\nAttendance information after program stopped:")
-print(df_attendance)
+# print("\nAttendance information after program stopped:")
+# print(df_attendance)
 
 for i in range(1):
     cv2.destroyAllWindows()
---
+++ learn_features.py
@@ -0,0 +1,40 @@
+'''
+Use this script to learn facial features of persons after 
+having images of all persons stored in `photos` directory.
+
+All the embeddings and features will be stored in the data.py
+file that is loaded in direct_use.py
+'''
+
+from facenet_pytorch import MTCNN, InceptionResnetV1
+import torch
+from torchvision import datasets
+from torch.utils.data import DataLoader
+from PIL import Image
+
+# Initializing MTCNN and InceptionResnetv1
+mtcnn0 = MTCNN(image_size=240, margin=0, keep_all=False, min_face_size=40) # keep_all = False means if one img contains many faces then it will keep only 1 face of those
+resnet = InceptionResnetV1(pretrained='vggface2').eval() # creating an instance of pre-trained InceptionResnetV1 model trained on the VGGFace2 dataset to extract embeddings
+
+# Read data from directory
+dataset = datasets.ImageFolder('photos') # photos directory path
+idx_to_class = {i:c for c,i in dataset.class_to_idx.items()} # new dictionary of indecis and class names
+
+def collate_fn(x):
+    return x[0]
+
+loader = DataLoader(dataset, collate_fn=collate_fn)
+
+name_list = [] # list of names corresponding to cropped photos
+embedding_list = [] # list of embedding matrix after conversion from cropped faces to embedding matrix using resnet
+
+for img, idx in loader: # img is in PIL format, here we are looping thru the images in the photos directory (loader)
+    face, prob = mtcnn0(img, return_prob=True) # img passed into mtcnn0, face is the cropped face (note: in mtcnn0 it will return only ONE face)
+    if face is not None and prob>0.92: # if face is not None means that there's a face that exists 
+        emb = resnet(face.unsqueeze(0)) # pass the face into resnet, unsqueeze b/c resnet expects 4 dimensions (dimension of batch included). Result of this is an embedding
+        embedding_list.append(emb.detach()) # .detach() to make requires_grad false
+        name_list.append(idx_to_class[idx])
+
+# save data
+data = [embedding_list, name_list] # make a new list of the previous 2 lists
+torch.save(data, 'data.pt') # saving to make this code reusable without having to go thru the previous loops and steps every single time which is very costly
